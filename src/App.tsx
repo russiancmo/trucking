d876a12a33697e1d82798c49f8532cdf76a86066
ligadobra <russiancmo@gmail.com>
2022-10-06 10:51:30 +0300

    create project with leaflet

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+
+const position = [51.505, -0.09]
+
+function App() {
+  return (
+    <MapContainer center={{lat: 55.751244,lng: 37.618423}} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={{lat: 55.751244,lng: 37.618423}}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default App;

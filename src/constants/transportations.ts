399990bc196a07de153ca7d2c2099efa78344e77
ligadobra <russiancmo@gmail.com>
2022-10-08 02:23:40 +0300

    add constants to maps main layer

---
+++ src/constants/transportations.ts
@@ -0,0 +1,8 @@
+import { TMapProperties } from "../types/map-properties";
+
+export const MapProperties: TMapProperties = {
+  center: { lat: 55.751244, lng: 37.618423 },
+  zoom: 13,
+  scrollWheelZoom: false,
+  tileLayer: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+};

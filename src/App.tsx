2e9b4adc298ef7941f8e125dedda11ae9c0d69a4
ligadobra <russiancmo@gmail.com>
2022-10-08 02:22:29 +0300

    change app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Transportation } from "./components/Transportations/transportation";
+import { Provider } from "react-redux";
+import store from "./store/init";
+import styles from "./App.module.scss";
+import "antd/dist/antd.min.css";
 import "./App.css";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 function App() {
   return (
-    <MapContainer center={{lat: 55.751244,lng: 37.618423}} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={{lat: 55.751244,lng: 37.618423}}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <Provider store={store}>
+      <div className={styles.container}>
+        <Transportation />
+      </div>
+    </Provider>
   );
 }
 
